de1aed8afdc5a90a368e08878af01ca1be658d2b
Michael Haynie <michael.dean.haynie@gmail.com>
2022-09-02 16:04:33 -0700

    start building out the step functions

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import { buildBoardMarkup } from './modules/build-board-markup.mjs'
 import { log } from './modules/log.mjs'
 
-buildBoardMarkup()
-
 const events = JSON.parse(log);
-events.forEach(event => {
-    playEvent(event)
-})
+let eventsCursor = -1;
 
-function playEvent(event) {
+buildBoardMarkup()
+bindButtons()
+
+function applyEvent(event) {
     const { hints } = event
     for(let hintIdx = 0; hintIdx < hints.length; hintIdx++) {
         const hint = hints[hintIdx]
@@ -21,10 +20,48 @@ function playEvent(event) {
     }
 }
 
+function revertEvent(event) {
+    //pu@ while I'm applying an event, build up an array of revert functions/params to call to revert
+}
+
 function setCellValue(cellId, cellValue) {
     const cellDiv = document.getElementById(cellId)
     cellDiv.classList.add('solved')
 
     const cellValueDiv = document.getElementById(`${cellId}v`)
     cellValueDiv.innerText = cellValue
+}
+
+function stepForward() {
+    eventsCursor++
+    applyEvent(events[eventsCursor])
+    updateButtons()
+}
+
+function stepBackward() {
+    revertEvent(events[eventsCursor])
+    eventsCursor--
+    updateButtons()
+}
+
+function updateButtons() {
+    // Update "Step Backward" button
+    if (eventsCursor === -1) {
+        document.getElementById('stepBackward').setAttribute('disabled', 'true')
+    } else {
+        document.getElementById('stepBackward').removeAttribute('disabled')
+    }
+
+    // Update "Step Forward" button
+    if (eventsCursor === (events.length - 1)) {
+        document.getElementById('stepForward').setAttribute('disabled', 'true')
+    } else {
+        document.getElementById('stepForward').removeAttribute('disabled')
+    }
+}
+
+function bindButtons() {
+    document.getElementById('stepForward').onclick = stepForward
+    document.getElementById('stepBackward').onclick = stepBackward
+    updateButtons();
 }
