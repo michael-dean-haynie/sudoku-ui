a6860a13f6897a44f1e65d11e734cae76b9a759f
Michael Haynie <michael.dean.haynie@gmail.com>
2022-09-01 18:22:15 -0700

    build up starting board markup and classes for styles

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import { log } from './modules/log.mjs'
+
 const ROWS = 9;
 const COLS = 9;
+const NOTES = 9;
 
-console.log('hello world')
+console.log('hello worlds')
+console.log(log || 'could not find log');
 buildBoardMarkup()
 
 function buildBoardMarkup() {
@@ -10,9 +14,36 @@ function buildBoardMarkup() {
 
     for(let row = 0; row < ROWS; row++) {
         for(let col = 0; col < COLS; col++) {
-            const cellDiv = document.createElement('cell');
+            const cellDiv = document.createElement('div')
             cellDiv.id = `c${row}${col}`
+            cellDiv.classList.add('cell')
             boardDiv.appendChild(cellDiv)
+
+            for(let note = 1; note <= NOTES; note++) {
+                const noteDiv = document.createElement('div')
+                noteDiv.id = `${cellDiv.id}n${note}`
+                noteDiv.classList.add('note')
+                noteDiv.innerText = note
+                cellDiv.appendChild(noteDiv)
+
+
+                // TODO: remove
+                // if (cellDiv.id === 'c62' && note < 4) {
+                //     noteDiv.classList.add('eliminated')
+                // }
+            }
+
+            const valueDiv = document.createElement('div')
+            valueDiv.id=`${cellDiv.id}v`
+            valueDiv.classList.add('value')
+            valueDiv.innerText = 0
+            cellDiv.appendChild(valueDiv)
+
+
+            // TODO: remove
+            // if(cellDiv.id === 'c45') {
+            //     cellDiv.classList.add('solved')
+            // }
         }
     }
 }
