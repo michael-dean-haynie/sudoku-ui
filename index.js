015306f42c2b9d2f1f1b5307782e6a2b929ad3c5
Michael Haynie <michael.dean.haynie@gmail.com>
2022-09-03 20:26:41 -0700

    add rewind, pause, play buttons

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import { context } from './modules/initialize-context.js'
 import { eventHandlers } from './modules/event-handlers/event-handlers.js'
 
 const events = JSON.parse(log)
-let cursor = -1;
+let cursor = -1
+let rewindPausePlay = 'pause'
+let rewindPlayIntervalId
+let rewindPlayInterval = 100
 
 buildBoardMarkup()
 bindButtons()
@@ -20,35 +23,91 @@ function revertEvent(event) {
 }
 
 function stepForward() {
-    cursor++
-    applyEvent(events[cursor], context)
+    if(cursor !== events.length -1) {
+        cursor++
+        applyEvent(events[cursor], context)
+    }
+    if(cursor === events.length -1) {
+        pause()
+    }
     updateButtons()
 }
 
 function stepBackward() {
-    revertEvent(events[cursor])
-    cursor--
+    if(cursor !== -1) {
+        revertEvent(events[cursor])
+        cursor--
+    }
+    if(cursor === -1) {
+        pause()
+    }
     updateButtons()
+}
+
+function rewind() {
+    rewindPausePlay = 'rewind'
+    rewindPlayIntervalId = setInterval(stepBackward, rewindPlayInterval)
+    stepBackward() // start first one immediately
+}
+
+function pause() {
+    rewindPausePlay = 'pause';
+    if(rewindPlayIntervalId) {
+        clearInterval(rewindPlayIntervalId);
+        rewindPlayIntervalId = null;
+        updateButtons()
+    }
+}
+
+function play() {
+    rewindPausePlay = 'play'
+    rewindPlayIntervalId = setInterval(stepForward, rewindPlayInterval)
+    stepForward() // start first one immediately
 }
 
 function updateButtons() {
     // Update "Step Backward" button
-    if (cursor === -1) {
+    if (cursor === -1 || rewindPausePlay !== 'pause') {
         document.getElementById('stepBackward').setAttribute('disabled', 'true')
     } else {
         document.getElementById('stepBackward').removeAttribute('disabled')
     }
 
     // Update "Step Forward" button
-    if (cursor === (events.length - 1)) {
+    if (cursor === (events.length - 1) || rewindPausePlay !== 'pause') {
         document.getElementById('stepForward').setAttribute('disabled', 'true')
     } else {
         document.getElementById('stepForward').removeAttribute('disabled')
     }
+
+    // Update "Rewind" button
+    if(rewindPausePlay !== 'pause' || cursor === -1) {
+        document.getElementById('rewind').setAttribute('disabled', 'true')
+    } else {
+        document.getElementById('rewind').removeAttribute('disabled')
+    }
+
+    // update "Pause" button
+    if(rewindPausePlay === 'pause') {
+        document.getElementById('pause').setAttribute('disabled', 'true')
+    } else {
+        document.getElementById('pause').removeAttribute('disabled')
+    }
+
+    // update "Play" button
+    if(rewindPausePlay !== 'pause' || cursor === (events.length -1)) {
+        document.getElementById('play').setAttribute('disabled', 'true')
+    } else {
+        document.getElementById('play').removeAttribute('disabled')
+    }
+
 }
 
 function bindButtons() {
     document.getElementById('stepForward').onclick = stepForward
     document.getElementById('stepBackward').onclick = stepBackward
+    document.getElementById('rewind').onclick = rewind
+    document.getElementById('pause').onclick = pause
+    document.getElementById('play').onclick = play
     updateButtons();
 }
